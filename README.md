5be6754d5cc91e0291c1f28f9cd9ecb51e09c374
mykldog7 <michael@Extension.localdomain>
2023-06-22 22:36:26 +1200

    Basic JSON parse

---
+++ README.md
@@ -0,0 +1,13 @@
+# Heliostat Project
+
+Work in progress... 
+
+Basic architecture:
+
+[Robot] <-> [GRBL on Arduion] <-> [RPI] <-> Site/App?
+
+Robot has two steppers, each controlling azimuth and altitude of a mirror. 
+
+GRBL is used to send signals to the driver boards.
+
+GRBL recieves GCODE from RPI, which hosts the ws server(and serves a control panel site)
---
+++ cmd/sun/config.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+)
+
+// Config is used to store the core configuration of the heliostat at the present time.
+// example: {"name":"test", "loc":{"lat": 130, "long": -42}}
+type Config struct {
+	Name     string `json:"name"`
+	Location struct {
+		Lat  float64 `json:"lat"`
+		Long float64 `json:"long"`
+	} `json:"loc"`
+}
+
+func (c Config) String() string {
+	s, err := json.Marshal(c)
+	if err != nil {
+		panic(err)
+	}
+	return string(s)
+}
